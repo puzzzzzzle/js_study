e6747148b1194a399c7fb88e095e2fbfeb46966f
puzzzzzzle <2359173906@qq.com>
2023-08-11 16:27:30 +0800

    update

--- web_ts_language/hello_world.ts
+++ web_ts_language/hello_world.ts
@@ -4,3 +4,12 @@ function hello(name: string, id: string): void {
     console.log(`will update ${id} by ${name}`)
     document.getElementById(id).innerHTML = `hello from ${name}`
 }
+
+document.getElementById("bt_hello").addEventListener("click", function () {
+    hello("func", 'hello_text');
+}, false);
+document.getElementById("bt_input_hello").addEventListener("click", function () {
+    const t = (document.getElementById("in_input_hello") as HTMLInputElement).value;
+    console.log(t)
+    hello(t, 'hello_text');
+}, false);
