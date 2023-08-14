c411a793790261774bcc83c32fb2e7d006cecfd1
puzzzzzzle <2359173906@qq.com>
2023-08-14 20:28:32 +0800

    add

--- viteVueStudy/src/components/HelloWorld.vue
+++ viteVueStudy/src/components/HelloWorld.vue
@@ -8,32 +8,35 @@ const count = ref(0)
 function onClick() {
   console.log("on btn click")
   count.value++
+  $("#hello_world").children("p[name='edit_para']").hide()
 }
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
+  <div id="hello_world">
+    <h1>{{ msg }}</h1>
     <button type="button" @click="onClick()">count is {{ count }}</button>
-    <p>
+    <p name = "edit_para">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
+    <p name = "edit_para">
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+    <p>
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+      >, the official Vue + Vite starter
+    </p>
+    <p>
+      Install
+      <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+      in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-    >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
--- viteVueStudy/src/main.ts
+++ viteVueStudy/src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+
+import * as $ from 'jquery'
+
+//@ts-ignore
+window.jQuery = window.$ = $
 
 createApp(App).mount('#app')
