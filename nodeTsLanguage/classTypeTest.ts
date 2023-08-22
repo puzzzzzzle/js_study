31589530af53e1b86b2b14134435fab3b96616cb
puzzzzzzle <2359173906@qq.com>
2023-08-22 20:36:32 +0800

    add

---
+++ nodeTsLanguage/classTypeTest.ts
@@ -0,0 +1,53 @@
+class Human {
+    // private
+    static newCount = 0;
+    age = 10;
+    name = "";
+
+    constructor(name: string, age = 10) {
+        this.name = name
+        this.age = age
+        Human.newCount++
+    }
+
+    hello() {
+        console.log(`hello from ${this.name}`)
+    }
+
+    static get count() {
+        return this.newCount
+    }
+}
+
+let h = new Human("aaa", 11)
+h.hello()
+let h1 = new Human("bbb")
+h1.hello()
+console.log(`curr created count ${Human.count}`)
+
+class Student extends Human {
+    school: string;
+
+    constructor(name: string, age: number, school: string) {
+        super(name, age)
+        this.school = school
+    }
+
+    hello() {
+        console.log(`hello from student ${this.name} ${this.school}`)
+    }
+}
+
+let s = new Student("sss", 111, "sch1")
+s.hello()
+console.log(`curr created count ${Human.count}`)
+console.log(`curr created count ${Student.count}`)
+
+
+class Data {
+    d1 = "sss"
+    d2: string
+    d3 = 11
+}
+
+let d = new Data()
